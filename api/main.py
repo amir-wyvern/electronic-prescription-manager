061b685f018c86c61e55556445b0ae54f1866ede
wyvern <amirhosein_wyvern@yahoo.com>
2021-12-28 15:49:13 +0100

    fix error

--- api/main.py
+++ api/main.py
@@ -11,10 +11,10 @@ sys.path.insert(0, dire )
 from routers.v1 import patient  as patient_v1
 from routers.v1 import login    as login_v1
 from routers.v1 import services as services_v1
-from routers.v1 import doctor   as doctor_v1
+# from routers.v1 import doctor   as doctor_v1
 from routers.v1 import visit    as visit_v1
 
-from async_redis.redis_orm import redis
+from async_redis.redis_obj import redis
 from fastConfig import Swagger
 from fastapi.middleware.cors import CORSMiddleware
 
