f7557564f4f2cf64af4b9058f6ee8bc6bbdae325
wyvern <amirhosein_wyvern@yahoo.com>
2022-01-06 21:22:14 +0100

    just for test

--- api/main.py
+++ api/main.py
@@ -24,8 +24,8 @@ from dotenv import dotenv_values
 config = dotenv_values('.env')
 
 
-# REDIS_HOST = config['REDISURI']
-REDIS_HOST = 'localhost'
+REDIS_HOST = config['REDISURI']
+# REDIS_HOST = 'localhost'
 
 REDIS_PORT = '6379'
 
